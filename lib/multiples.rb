9104ae51c04601f17106cabf8c035734a3097033
ClaFiore <claborghini@gmail.com>
2020-10-27 19:35:45 -0400

    added method

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
-# Enter your procedural solution here!
+def mult3and5(x)
+    
+    multiples = []
+    (1..x).each do |num|
+        if num % 3 == 0 || num % 5 == 0
+            multiples.push(num)
+        end
+    end
+    puts multiples
+end
+
+x = 10
+mult3and5(x)
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,49 @@
-# Enter your object-oriented solution here!
+class Multiples
+    @@all = []
+    
+    def initialize(num)
+        @num = num
+    end
+
+    def mult_of_3?
+        if @num % 3 == 0
+            return true
+        else
+            return false
+        end
+    end
+
+    def mult_of_5?
+        if @num % 5 == 0
+            return true
+        else
+            return false
+        end
+    end
+
+    def mult_3_or_5?
+        if mult_of_3? || mult_of_5?
+            return true
+        else
+            false
+        end
+    end
+
+    def self.all_multiples
+        if mult_3_or_5?
+            @@all << self
+            return @@all
+        end
+    end
+
+    def sum
+        self.all_multiples.sum
+    end
+
+end
+
+(1..10).each do |num|
+    Multiples.new(num)
+end
+
+puts Multiples.all_multiples
